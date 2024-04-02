66b161361b379c79a4c4c9063ed59a3cc681163e
Victor Revenco <victor.revenco@isa.utm.md>
2024-04-02 11:58:59 +0300

    Add Lab3

--- Lab2.py
+++ Lab2.py
@@ -52,12 +52,12 @@ def merge(left, right):
 
 def heapify(arr, n, i):
     largest = i
-    l = 2 * i + 1
-    r = 2 * i + 2
-    if l < n and arr[i] < arr[l]:
-        largest = l
-    if r < n and arr[largest] < arr[r]:
-        largest = r
+    left = 2 * i + 1
+    right = 2 * i + 2
+    if left < n and arr[i] < arr[left]:
+        largest = left
+    if right < n and arr[largest] < arr[right]:
+        largest = right
     if largest != i:
         arr[i], arr[largest] = arr[largest], arr[i]
         heapify(arr, n, largest)
